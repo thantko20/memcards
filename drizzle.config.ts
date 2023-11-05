09ecd70d885e8fd5b550979892fa8caa77336769
Thant Ko Zaw <thantmkou10@gmail.com>
2023-11-05 20:32:23 +0630

    Init and testing

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "drizzle-kit";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: __dirname + "/.env" });
+
+export default {
+  schema: "./src/lib/db/schema.ts",
+  driver: "pg",
+  dbCredentials: {
+    connectionString: process.env.DATABASE_URL as string
+  },
+  out: "./drizzle"
+} satisfies Config;
