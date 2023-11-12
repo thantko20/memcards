3411af2f9214fc01d998e0818e3dd615cb4ad24f
Thant Ko Zaw <thantmkou10@gmail.com>
2023-11-12 15:13:31 +0630

    Remove .env

--- src/app/(auth-layout)/layout.tsx
+++ src/app/(auth-layout)/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="max-w-[500px] mx-auto pt-12">
       <ThemeToggle className="absolute top-4 right-4 md:top-12 md:right-12 z-30" />
-      <main className="relative z-20">{children}</main>
+      <main className="relative z-20 px-2">{children}</main>
     </div>
   );
 }
